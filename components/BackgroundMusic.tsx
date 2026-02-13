b1caf882eb3467525e8236c4b2d893000adea1e8
Girikumaresan007 <girikumaresan007@gmail.com>
2026-02-13 11:00:30 +0530

    Fix background music and update vite base path

--- components/BackgroundMusic.tsx
+++ components/BackgroundMusic.tsx
@@ -34,7 +34,7 @@ export const BackgroundMusic: React.FC = () => {
 
       {/* Speaker button (user click starts music) */}
       {showIcon && (
-        <div className="fixed bottom-6 right-6 z-50">
+      <div className="fixed bottom-4 right-4 sm:bottom-6 sm:right-6 z-50">
           <button
             onClick={() => {
               if (audioRef.current) {
